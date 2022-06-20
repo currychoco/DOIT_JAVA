c2f4d7f782666146d29d8a82916827b7daced102
currychoco <godqhrgka0000@naver.com>
2022-06-20 15:57:43 +0900

    관계 연산자를 이용한 조건 연산자 학습

---
+++ src/chapter3/OperatorEx4_1.java
@@ -0,0 +1,15 @@
+package chapter3;
+
+public class OperatorEx4_1 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		boolean isEven;
+		
+		isEven = (num % 2 == 0) ? true : false;
+		
+		System.out.println(isEven);
+
+	}
+
+}
