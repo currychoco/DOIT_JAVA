7f5b0d864139f9cfa4a5d5d328dc4b5d30c54b37
currychoco <godqhrgka0000@naver.com>
2022-06-20 14:26:28 +0900

    여러 형태의 변수를 이용해 봄. 유니코드를 사용하여 문자를 표시함

---
+++ src/chapter2/Test1.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		float num2 = 2.0f;
+		int num3 = (int)(num1 + num2);
+		
+		System.out.println(num3);
+
+	}
+
+}
---
+++ src/chapter2/Test2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		char ch1 = '글';
+		char ch2 = '\uAE00';
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+
+}
