7b20eead0bbe120b3ec2e37ff03f6b7190387526
currychoco <godqhrgka0000@naver.com>
2022-06-20 16:48:36 +0900

    단항 연산자와 산술 연산자의 이해

---
+++ src/chapter3/Q3.java
@@ -0,0 +1,18 @@
+package chapter3;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(num);
+		System.out.println(num + 1);
+		System.out.println(num);
+		System.out.println(num++);
+		System.out.println(num);
+		System.out.println(--num);
+		System.out.println(--num);
+		System.out.println(num);
+
+	}
+
+}
