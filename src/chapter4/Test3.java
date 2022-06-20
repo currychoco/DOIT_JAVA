8212682a253c5397e3ee26e75255da70faad5d4d
currychoco <godqhrgka0000@naver.com>
2022-06-20 18:59:15 +0900

    for문을 이용한 반복문 연습

---
+++ src/chapter4/Test3.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		int num;
+		
+		for(num = 1; num <=10; num++) {
+			System.out.println("안녕하세요" + num);
+		}
+
+	}
+
+}
