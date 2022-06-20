43634421f4bb4197d8b5761682902944c8d512d0
currychoco <godqhrgka0000@naver.com>
2022-06-20 14:54:52 +0900

    단항 연산자 위치 변경시 일어나는 결과 확인

--- src/chapter3/OperationEx2.java
+++ src/chapter3/OperationEx2.java
@@ -5,11 +5,17 @@ public class OperationEx2 {
 	public static void main(String[] args) {
 		int gameScore = 150;
 		
-		int lastScore1 = ++gameScore;
+		int lastScore1 = gameScore++;
 		System.out.println(lastScore1);
-		
-		int lastScore2 = --gameScore;
+		 
+		int lastScore2 = gameScore--;
 		System.out.println(lastScore2);
+		
+		int lastScore3 = gameScore--;
+		System.out.println(lastScore3);
+		
+		int lastScore4 = gameScore;
+		System.out.println(lastScore4);
 		
 
 	}
