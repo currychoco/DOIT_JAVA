eec34f15a41b258dc20cfe6c2bc9706d4de9909f
currychoco <godqhrgka0000@naver.com>
2022-06-24 16:01:21 +0900

    상속의 다형성 학습

---
+++ src/polymorphism/Customer.java
@@ -0,0 +1,60 @@
+package polymorphism;
+
+public class Customer {
+	
+	protected int customerID;
+	protected String customeraName;
+	protected String customerGrade;
+	int bonusPoint;
+	double bonusRatio;
+	
+	public Customer() {
+		initCustomer();
+	}
+	
+	public Customer(int customerID, String customerName) {
+		this.customerID = customerID;
+		this.customeraName = customerName;
+		initCustomer();
+		//System.out.println("Customer() 생성자 호출");
+	}
+	
+	private void initCustomer() {
+		customerGrade = "SILVER";
+		bonusRatio = 0.01;
+	}
+	
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price;
+	}
+	
+	public String showCustomerInfo() {
+		return customeraName + "님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";
+	}
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;		
+	}
+	
+	public String getCustomerName() {
+		return customeraName;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customeraName = customerName;
+	}
+	
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+	
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade = customerGrade;
+	}
+
+}
