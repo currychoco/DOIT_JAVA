f67601eb97dd0857a292fdfa6ec86afcfe6def2b
currychoco <godqhrgka0000@naver.com>
2022-06-27 19:12:57 +0900

    String과 Integer의 toString()메서드와 equals()메서드 학습, equals메서드 재정의 학습

---
+++ src/object/AloneTest.java
@@ -0,0 +1,43 @@
+package object;
+
+class MyDate{
+	int day;
+	int month;
+	int year;
+	
+	public MyDate(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof MyDate) {
+			MyDate myd = (MyDate)obj;
+			if((this.day == myd.day) && (this.month == myd.month) && (this.year == myd.year)) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+public class AloneTest {
+
+	public static void main(String[] args) {
+		
+		MyDate myd1 = new MyDate(27, 6, 2022);
+		MyDate myd2 = new MyDate(27, 6, 2022);
+		MyDate myd3 = new MyDate(27, 6, 2024);
+		
+		System.out.println(myd1.equals(myd2));
+		System.out.println(myd1.equals(myd3));
+
+	}
+
+}
